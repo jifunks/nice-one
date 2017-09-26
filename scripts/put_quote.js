29d9385970c1853fb5fc480d98ac8d145d252071
Jake Funke <jifunks@gmail.com>
2017-09-26 17:52:16 +0000

    Fix formatting, switch quote source

--- scripts/put_quote.js
+++ scripts/put_quote.js
@@ -3,13 +3,13 @@ pushQuote = function(callback) {
   request.onload = function() {
       var fileContent = this.responseText;
       // need to filter bc of empty element
-      var fileContentLines = fileContent.split( '\n' ).filter(i => i);
+      var fileContentLines = fileContent.split( '---' ).filter(i => i);
       var randomLineIndex = Math.floor(Math.random() * fileContentLines.length);
       var randomLine = fileContentLines[ randomLineIndex ];
       document.getElementById('text').innerHTML = randomLine;
       callback();
   };
-  request.open('GET', 'quotes.txt', true);
+  request.open('GET', 'karlen_quotes.txt', true);
   request.send();
 }
 
